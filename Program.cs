d160559e6d58de192e5e468e40a1b7bb766bcb94
mlundberg93 <Marcusluundbergh@gmail.com>
2023-10-31 17:26:01 +0100

    Lagt till metod LoadList för att göra det lättläsligare i Main koden

--- Program.cs
+++ Program.cs
@@ -24,74 +24,7 @@
                 }
                 else if (commandLine[0] == "load")
                 {
-                    if (commandLine.Length < 2)
-                    {
-                        lastFileName = "address.lis";
-
-
-                        using (StreamReader infile = new StreamReader(lastFileName))  //TODO felhantering vid fel filnamn
-                        {
-                            string line;
-                            while ((line = infile.ReadLine()) != null)
-                            {
-                                Console.WriteLine(line);
-                                string[] attrs = line.Split('|');
-                                Person p = new Person();
-                                p.persname = attrs[0];
-                                p.surname = attrs[1];
-                                string[] phones = attrs[2].Split(';');
-                                p.phone = phones[0];
-                                string[] addresses = attrs[3].Split(';');
-                                p.address = addresses[0];
-                                for (int ix = 0; ix < contactList.Length; ix++)
-                                {
-                                    if (contactList[ix] == null)
-                                    {
-                                        contactList[ix] = p;
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-
-                    }
-
-                    else
-                    {
-                        lastFileName = commandLine[1];
-                        try
-                        {
-                            using (StreamReader infile = new StreamReader(lastFileName))
-                            {
-                                string line;
-                                while ((line = infile.ReadLine()) != null)
-                                {
-                                    Console.WriteLine(line);
-                                    string[] attrs = line.Split('|');
-                                    Person p = new Person();
-                                    p.persname = attrs[0];
-                                    p.surname = attrs[1];
-                                    string[] phones = attrs[2].Split(';');
-                                    p.phone = phones[0];
-                                    string[] addresses = attrs[3].Split(';');
-                                    p.address = addresses[0];
-                                    for (int ix = 0; ix < contactList.Length; ix++)
-                                    {
-                                        if (contactList[ix] == null)
-                                        {
-                                            contactList[ix] = p;
-                                            break;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        catch (System.IO.FileNotFoundException exc)
-                        {
-                            Console.WriteLine(exc.Message);
-                        }
-
-                    }
+                    lastFileName = LoadList(commandLine);
                 }
                 else if (commandLine[0] == "save")
                 {
@@ -131,17 +64,7 @@
                 }
                 else if (commandLine[0] == "help")
                 {
-                    Console.WriteLine("Avaliable commands: ");
-                    Console.WriteLine("  delete       - emtpy the contact list");
-                    Console.WriteLine("  delete /persname/ /surname/ - delete a person");
-                    Console.WriteLine("  load        - load contact list data from the file address.lis");
-                    Console.WriteLine("  load /file/ - load contact list data from the file");
-                    Console.WriteLine("  new        - create new person");
-                    Console.WriteLine("  new /persname/ /surname/ - create new person with personal name and surname");
-                    Console.WriteLine("  quit        - quit the program");
-                    Console.WriteLine("  save         - save contact list data to the file previously loaded");
-                    Console.WriteLine("  save /file/ - save contact list data to the file");
-                    Console.WriteLine();
+                    MenuPrinter();
                 }
                 else
                 {
@@ -160,7 +83,82 @@
                 Console.WriteLine("  save         - save contact list data to the file previously loaded");
                 Console.WriteLine("  save /file/ - save contact list data to the file");
                 Console.WriteLine();
+            } //En metod för att samla ihop då den används på mer än ett ställe.
+
+        }
+
+        private static string LoadList(string[] commandLine)
+        {
+            string lastFileName;
+            if (commandLine.Length < 2)
+            {
+                lastFileName = "address.lis";
+
+
+                using (StreamReader infile = new StreamReader(lastFileName))  //TODO felhantering vid fel filnamn
+                {
+                    string line;
+                    while ((line = infile.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                        string[] attrs = line.Split('|');
+                        Person p = new Person();
+                        p.persname = attrs[0];
+                        p.surname = attrs[1];
+                        string[] phones = attrs[2].Split(';');
+                        p.phone = phones[0];
+                        string[] addresses = attrs[3].Split(';');
+                        p.address = addresses[0];
+                        for (int ix = 0; ix < contactList.Length; ix++)
+                        {
+                            if (contactList[ix] == null)
+                            {
+                                contactList[ix] = p;
+                                break;
+                            }
+                        }
+                    }
+                }
+
             }
+            else
+            {
+                lastFileName = commandLine[1];
+                try
+                {
+                    using (StreamReader infile = new StreamReader(lastFileName))
+                    {
+                        string line;
+                        while ((line = infile.ReadLine()) != null)
+                        {
+                            Console.WriteLine(line);
+                            string[] attrs = line.Split('|');
+                            Person p = new Person();
+                            p.persname = attrs[0];
+                            p.surname = attrs[1];
+                            string[] phones = attrs[2].Split(';');
+                            p.phone = phones[0];
+                            string[] addresses = attrs[3].Split(';');
+                            p.address = addresses[0];
+                            for (int ix = 0; ix < contactList.Length; ix++)
+                            {
+                                if (contactList[ix] == null)
+                                {
+                                    contactList[ix] = p;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (System.IO.FileNotFoundException exc)
+                {
+                    Console.WriteLine(exc.Message);
+                }
+
+            }
+
+            return lastFileName;
         }
     }
 }
